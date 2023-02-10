d25107e611a41f7745a685dae600730b5e04e4ac
Eugene2077 <aosk@mail.com>
2023-02-10 14:44:05 -0500

    ICE4_update

---
+++ ICE4/scripts/user.js
@@ -0,0 +1,38 @@
+// Course : WEBD6201
+// STUDENT NUMBER : 100844220
+// NAME: Eugene (Hongju) Shin
+// Date: 10 Feb 2023 
+
+
+/*
+* Class to create new user
+ */
+export class User {
+
+    /**
+     * User
+     * @param {string} firstName
+     * @param {string} lastName
+     * @param {string} username
+     * @param {string} email
+     * @param {string} password
+     */
+    constructor(firstName, lastName, username, email, password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    /**
+     * @property {function} displayUser Prints users details: user name, username, email, and password
+     * @returns {string}
+     */
+    displayUser() {
+         return `Name: ${this.firstName}${this.lastName}
+         Username: ${this.username}
+         Email: ${this.email}
+         Password: ${this.password}`
+    }
+}
